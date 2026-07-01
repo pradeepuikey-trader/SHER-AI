fa38ed917f4234fba14c5405b7921b1fcfafa28a
pradeepuikey-trader <uikeyji2442@gmail.com>
2026-07-01 23:19:43 +0530

    Update gemini_ai.py

--- gemini_ai.py
+++ gemini_ai.py
@@ -1,36 +1,52 @@
 import streamlit as st
 import google.generativeai as genai
+import json
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
-
 def analyze_image(image):
 
     prompt = """
-    You are a professional Smart Money Concept trader.
+You are an expert Smart Money Concept trader.
 
-    Analyze this TradingView chart.
+Analyze this TradingView screenshot.
 
-    Return ONLY in this format:
+Return ONLY valid JSON.
 
-    Trend:
-    Entry:
-    Stop Loss:
-    Target 1:
-    Target 2:
-    Confidence:
-    BOS:
-    CHoCH:
-    Liquidity Sweep:
-    Order Block:
-    Fair Value Gap:
-    Reason:
-    """
+No markdown.
+No explanation.
+No ```json.
+
+JSON format:
+
+{
+    "trend":"",
+    "entry":"",
+    "stoploss":"",
+    "target1":"",
+    "target2":"",
+    "confidence":"",
+    "bos":true,
+    "choch":true,
+    "liquidity":true,
+    "orderblock":true,
+    "fvg":true,
+    "reason":""
+}
+"""
 
     response = model.generate_content(
-        [prompt, image]
+        contents=[prompt, image]
     )
 
-    return response.text
+    text = response.text.strip()
+
+    if text.startswith("```json"):
+        text = text.replace("```json", "").replace("```", "").strip()
+
+    elif text.startswith("```"):
+        text = text.replace("```", "").strip()
+
+    return json.loads(text)
