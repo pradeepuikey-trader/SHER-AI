e3eb9de3e2c119b1c6409dd34a673b568c719d97
pradeepuikey-trader <uikeyji2442@gmail.com>
2026-07-01 23:21:40 +0530

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -1,15 +1,18 @@
 from gemini_ai import analyze_image
 
 def analyze_chart(image):
+
     try:
-        response = analyze_image(image)
+
+        analysis = analyze_image(image)
 
         return {
             "success": True,
-            "analysis": response
+            "analysis": analysis
         }
 
     except Exception as e:
+
         return {
             "success": False,
             "analysis": str(e)
