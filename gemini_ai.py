f2bcd4a629cd7ba0bfe4dd24d15e9a4ce215580b
pradeepuikey-trader <uikeyji2442@gmail.com>
2026-07-01 22:22:56 +0530

    Update gemini_ai.py

--- gemini_ai.py
+++ gemini_ai.py
@@ -1,6 +1,36 @@
+import streamlit as st
 import google.generativeai as genai
 
-def setup_gemini(api_key):
-    genai.configure(api_key=api_key)
-    model = genai.GenerativeModel("gemini-2.5-flash")
-    return model
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+
+def analyze_image(image):
+
+    prompt = """
+    You are a professional Smart Money Concept trader.
+
+    Analyze this TradingView chart.
+
+    Return ONLY in this format:
+
+    Trend:
+    Entry:
+    Stop Loss:
+    Target 1:
+    Target 2:
+    Confidence:
+    BOS:
+    CHoCH:
+    Liquidity Sweep:
+    Order Block:
+    Fair Value Gap:
+    Reason:
+    """
+
+    response = model.generate_content(
+        [prompt, image]
+    )
+
+    return response.text
