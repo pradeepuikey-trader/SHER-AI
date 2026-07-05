79406a9bbd8bf95402e4303eab9d3f02dcb4daff
pradeepuikey-trader <uikeyji2442@gmail.com>
2026-07-05 11:26:16 +0530

    Update trade_score.py

--- trade_score.py
+++ trade_score.py
@@ -1,14 +1,34 @@
 def calculate_score(data):
-    score=0
-    for k in ["bos","choch","liquidity","orderblock","fvg"]:
-        if data.get(k):
-            score+=20
-    if score>=80:
-        signal="🟢 STRONG BUY"
-    elif score>=60:
-        signal="🟡 BUY"
-    elif score>=40:
-        signal="🟠 WAIT"
+
+    score = 0
+
+    if data["bos"]:
+        score += 20
+
+    if data["choch"]:
+        score += 20
+
+    if data["liquidity"]:
+        score += 20
+
+    if data["orderblock"]:
+        score += 20
+
+    if data["fvg"]:
+        score += 20
+
+    confidence = int(score)
+
+    if score >= 80:
+        signal = "🟢 STRONG BUY"
+
+    elif score >= 60:
+        signal = "🟢 BUY"
+
+    elif score >= 40:
+        signal = "🟡 WAIT"
+
     else:
-        signal="🔴 NO TRADE"
-    return score,signal
+        signal = "🔴 SELL"
+
+    return score, signal, confidence
